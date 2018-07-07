6b4ae89809b4c2c3bcd09b17f5f928dd0ab577cd
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-07-07 21:25:54 +0000

    createContext and empty context

--- index.js
+++ index.js
@@ -9,19 +9,25 @@ var NodeVM = require('vm2').NodeVM;
 var VMScript = require('vm2').VMScript;
 
 var code = 'var a = 123;';
+var thisContext = vm.createContext(this);
+var emptyContext = vm.createContext({});
 
 var suite = new Benchmark.Suite(`vm`);
 
 suite.add('eval', function() { eval(code); });
-suite.add('vm.runInContext context=this', function() { vm.runInContext(code, this); });
+suite.add('vm.runInContext context=this', function() { vm.runInContext(code, thisContext); });
+suite.add('vm.runInContext context=empty', function() { vm.runInContext(code, emptyContext); });
 suite.add('vm.runInNewContext', function() { vm.runInNewContext(code); });
 suite.add('vm.runInNewContext sandbox=this', function() { vm.runInNewContext(code, this); });
+suite.add('vm.runInNewContext sandbox=empty', function() { vm.runInNewContext(code, {}); });
 suite.add('vm.runInThisContext', function() { vm.runInThisContext(code); });
 
 var script = new vm.Script(code);
-suite.add('vm script.runInContext', function() { script.runInContext(this); });
+suite.add('vm script.runInContext context=this', function() { script.runInContext(thisContext); });
+suite.add('vm script.runInContext context=empty', function() { script.runInContext(emptyContext); });
 suite.add('vm script.runInNewContext', function() { script.runInNewContext(); });
 suite.add('vm script.runInNewContext sandbox=this', function() { script.runInNewContext(this); });
+suite.add('vm script.runInNewContext sandbox=empty', function() { script.runInNewContext({}); });
 suite.add('vm script.runInThisContext', function() { script.runInThisContext(); });
 
 var _vm1 = new VM();
